3ee5ce2ee8fe188bef4b0cb2e5c0e594fc0be931
Jason Plumb <jason@noisybox.net>
2014-07-14 21:27:42 -0700

    Respect robots.txt

--- crawlerUsingMechanize.py
+++ crawlerUsingMechanize.py
@@ -23,7 +23,7 @@ br.set_handle_equiv(True)
 br.set_handle_gzip(True)
 br.set_handle_redirect(True)
 br.set_handle_referer(True)
-br.set_handle_robots(False)
+br.set_handle_robots(True)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
 br.addheaders = [('User-agent', 
